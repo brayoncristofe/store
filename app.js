48376c5db91d78b51a174c423f1dd8bdd3946996
brayoncristofe <brayoncristofe28@gmail.com>
2026-05-21 23:04:33 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.get("/",function(req,res){
+    res.send("seja bem vindo")
+});
+
+app.get("/showcasegit",function(req,res){
+    db.query("SELECT * FROM products", function(err, results) {
+        if (err) {
+            return res.status(500).json({ error: "Erro ao buscar produtos" });
+        }
+
+        res.json(results);
+    });
+});
+
+app.listen(8081,function(){
+    console.log('seja bem vindo')
+});
